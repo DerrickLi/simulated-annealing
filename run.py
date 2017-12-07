d13f17d53d35ed0f4e9650c63e5c30c3e3dd5b7e
Derrick Li <derrickli@berkeley.edu>
2017-12-06 17:33:44 -0800

    fixed listdir bug and updated todo inputs

--- run.py
+++ run.py
@@ -7,9 +7,10 @@ def main(argv):
 		return
 	rootdir = argv[0]
 	for f in os.listdir(rootdir):
-		filepath = os.path.join(rootdir, f)
-		outpath = os.path.join(rootdir, os.path.splitext(f)[0] + ".out")
-		subprocess.call(["python", "solver.py", filepath, outpath])
+		if f.endswith(".in"):
+			filepath = os.path.join(rootdir, f)
+			outpath = os.path.join(rootdir, os.path.splitext(f)[0] + ".out")
+			subprocess.call(["python", "solver.py", filepath, outpath])
 
 if __name__=="__main__":
 	main(sys.argv[1:])
